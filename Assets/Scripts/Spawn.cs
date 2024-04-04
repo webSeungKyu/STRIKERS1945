54cb9fc39136c691336290eb8871fe76e2fddf9f
webSeungKyu <abcsksk1@gmail.com>
2024-04-04 13:39:20 +0900

    코루틴 이용하여 적 랜덤 생성 구현

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    public List<GameObject> monsterList;
+    [Header("몬스터 생성 x값 처음과 끝")]
+    public float startX = -2;
+    public float endX = 2;
+    [Header("몬스터 생성 시간")]
+    public float startTime = 1;
+    public float endTime = 20;
+    //public List<GameObject> monsterList;
 
+    [Header("몬스터와 생성 스위치")]
+    [SerializeField] private GameObject monster;
+    public bool spawnSwitch = true;
+    
     void Start()
     {
-        InvokeRepeating("MonsterSpawn", 3f, 1f);
+        StartCoroutine("RandomSpawn");
+        Invoke("StopSpawn", endTime);
+        //InvokeRepeating("MonsterSpawn", 3f, 1f);
     }
+
+    IEnumerator RandomSpawn()
+    {
+        while (spawnSwitch)
+        {
+            yield return new WaitForSeconds(startTime);
+            float x = Random.Range(startX, endX);
+            Vector2 r = new Vector2 (x, transform.position.y);
+
+            //몬스터 생성
+            Instantiate(monster, r, Quaternion.identity);
+        }
+    }
+
+    void StopSpawn()
+    {
+        spawnSwitch = false;
+        StopCoroutine("RandomSpawn");
+
+        //두 번째 몬스터
+    }
+    
+
 
     void Update()
     {
         
     }
 
-    void MonsterSpawn()
+    /*void MonsterSpawn()
     {
         Instantiate(monsterList[Random.Range(0, monsterList.Count)], new Vector2(Random.Range(-2, 3), transform.position.y), Quaternion.identity);
-    }
+    }*/
 }
