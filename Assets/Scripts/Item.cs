84977ec6b37a8880422326640df2dcaf9a6852f4
webSeungKyu <abcsksk1@gmail.com>
2024-04-04 12:34:32 +0900

    벽에 닿으면 튕기기 태그로 구현 > 머티리얼의 바운스로 수정
    CompareTag
    동적할당 없이 tag 비교가 가능하도록 구현된 메소드
    인자로 전달받은 string 이 실제로 존재하는지까지 확인하기 때문에 안정적
     - 예시로 [ gameOBject.tag == "tag" ] 사용 시 getter로 호출하고, string 을 복사 생성하기에 비효율적

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,51 +4,61 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-
-    public float moveSpeed = 1f;
-    Vector2 randomMove;
+    #region 벽에 닿으면 머리티얼의 바운스로 튕기게 하였으므로 아래 코드는 주석 처리
+    //public float moveSpeed = 1f;
+    //Vector2 randomMove;
+    #endregion
+    Rigidbody2D rb = null;
 
     void Start()
     {
-        randomMove = new Vector2(Random.Range(-1f, 2f), Random.Range(-1f, 2f));
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector3(Random.Range(42f, 97f), Random.Range(42f, 97f), 0f));
+        #region 벽에 닿으면 머리티얼의 바운스로 튕기게 하였으므로 아래 코드는 주석 처리
+        /*randomMove = new Vector2(Random.Range(-1f, 2f), Random.Range(-1f, 2f));
         while (randomMove.x == 0 && randomMove.y == 0)
         {
             randomMove = new Vector2(Random.Range(-1f, 2f), Random.Range(-1f, 2f));
-        }
+        }*/
+        #endregion
 
     }
 
     void Update()
     {
-        transform.Translate(randomMove * moveSpeed * Time.deltaTime);
+        //벽에 닿으면 머리티얼의 바운스로 튕기게 하였으므로 아래 코드는 주석 처리
+        //transform.Translate(randomMove * moveSpeed * Time.deltaTime);
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        #region 벽에 닿으면 튕기기
-        if (collision.tag == "LRWall")
+        #region 벽에 닿으면 튕기기 태그로 구현 > 머티리얼의 바운스로 수정
+        //
+        /*if (collision.CompareTag("LRWall"))
         {
             randomMove.x = randomMove.x * -1;
         }
-        else if (collision.tag == "UDWall")
+        else if (collision.CompareTag("UDWall"))
         {
             randomMove.y = randomMove.y * -1;
-        }
+        }*/
         #endregion
 
-        if(collision.tag == "Player" && gameObject.tag == "Power")
+        #region 아이템 먹었을 때 > 플레이어 스크립트에 넣는 것으로 수정하였음
+        /*if(collision.CompareTag("Player") && gameObject.CompareTag("Power"))
         {
             Player.power++;
             Destroy(gameObject);
             Debug.Log(Player.power);
         }
-        else if(collision.tag == "Player" && gameObject.tag == "Bomb")
+        else if(collision.CompareTag("Player") && gameObject.CompareTag("Bomb"))
         {
             Player.bomb++;
             Destroy(gameObject);
             Debug.Log(Player.bomb);
-        }
+        }*/
+        #endregion
     }
 
 
